be1ff9ea3de2a31b5aca448e54b55de429ba0d01
KlimVladMaks <vkm.blog@yandex.ru>
2025-06-29 20:34:26 +0300

    Commit 8

--- app/src/main/java/me/klimvlad/moneytransfer/screens/RegisterWindow.java
+++ app/src/main/java/me/klimvlad/moneytransfer/screens/RegisterWindow.java
@@ -3,6 +3,8 @@ package me.klimvlad.moneytransfer.screens;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -22,10 +24,45 @@ public class RegisterWindow implements Screen {
         Text title = new Text("Регистрация");
         title.setFont(Font.font(24));
         
-        Button backButton = new Button("Назад");
-        backButton.setOnAction(e -> onBackRequest.run());
+        // Логин
+        Text loginLabel = new Text("Введите уникальный логин (повторите ввод дважды):");
+        TextField loginField1 = new TextField();
+        loginField1.setPromptText("Логин");
+        loginField1.setMaxWidth(200);
         
-        VBox layout = new VBox(20, title, backButton);
+        TextField loginField2 = new TextField();
+        loginField2.setPromptText("Повторите логин");
+        loginField2.setMaxWidth(200);
+        
+        HBox loginFields = new HBox(10, loginField1, loginField2);
+        loginFields.setAlignment(Pos.CENTER);
+        
+        // Пароль
+        Text passwordLabel = new Text("Введите пароль (повторите ввод дважды):");
+        TextField passwordField1 = new TextField();
+        passwordField1.setPromptText("Пароль");
+        passwordField1.setMaxWidth(200);
+        
+        TextField passwordField2 = new TextField();
+        passwordField2.setPromptText("Повторите пароль");
+        passwordField2.setMaxWidth(200);
+        
+        HBox passwordFields = new HBox(10, passwordField1, passwordField2);
+        passwordFields.setAlignment(Pos.CENTER);
+        
+        // Кнопки
+        Button createAccountButton = new Button("Создать новый аккаунт");
+        createAccountButton.setPrefWidth(200);
+        
+        Button cancelButton = new Button("Отменить");
+        cancelButton.setPrefWidth(200);
+        cancelButton.setOnAction(e -> onBackRequest.run());
+        
+        VBox layout = new VBox(20, 
+            title, 
+            loginLabel, loginFields, 
+            passwordLabel, passwordFields, 
+            createAccountButton, cancelButton);
         layout.setAlignment(Pos.CENTER);
         
         Scene scene = new Scene(layout, 800, 600);
