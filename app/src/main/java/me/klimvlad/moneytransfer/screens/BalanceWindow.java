cae23697410dd45feb88f620a5f1e403fcc47f35
KlimVladMaks <vkm.blog@yandex.ru>
2025-08-18 21:22:05 +0300

    Commit 19

--- app/src/main/java/me/klimvlad/moneytransfer/screens/BalanceWindow.java
+++ app/src/main/java/me/klimvlad/moneytransfer/screens/BalanceWindow.java
@@ -2,6 +2,7 @@ package me.klimvlad.moneytransfer.screens;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -19,7 +20,12 @@ public class BalanceWindow implements Screen {
         Text title = new Text("Баланс");
         title.setFont(Font.font(24));
 
-        VBox layout = new VBox(20, title);
+        Button logoutButton = new Button("Выйти из аккаунта");
+        logoutButton.setStyle("-fx-background-color: transparent; -fx-text-fill: #0066cc; -fx-underline: true; -fx-font-size: 12px;");
+        logoutButton.setOnMouseEntered(e -> logoutButton.setStyle("-fx-background-color: transparent; -fx-text-fill: #004499; -fx-underline: true; -fx-font-size: 12px; -fx-cursor: hand;"));
+        logoutButton.setOnMouseExited(e -> logoutButton.setStyle("-fx-background-color: transparent; -fx-text-fill: #0066cc; -fx-underline: true; -fx-font-size: 12px; -fx-cursor: default;"));
+
+        VBox layout = new VBox(20, title, logoutButton);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout, 800, 600);
